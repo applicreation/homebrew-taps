1677f2b4f229720292a0559758288c233689f899
Gary Rutland <3715101+garyrutland@users.noreply.github.com>
2020-03-28 16:01:33 +0000

    Update aws-cli.rb

--- aws-cli.rb
+++ aws-cli.rb
@@ -4,7 +4,7 @@ class AwsCli < Formula
   homepage 'https://github.com/applicreation/aws-cli'
   url 'https://github.com/applicreation/aws-cli/releases/download/v0.3.0/aws-cli-darwin-amd64-0-3-0.tar.gz'
   version '0.3.0'
-  sha256 '4d7ec1e5e2644a3f530abac7ef1d7d7713ef1c7d866c609b9947d2d1f1d77fbb'
+  sha256 '808e01d79d29bd3986f0855988f36c375996a7b11fd0e243a5b1f55f32e9ee8b'
 
   depends_on :arch => :x86_64
 
