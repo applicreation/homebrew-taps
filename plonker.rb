3002b685b90bcee835e0dd98115ba3df948e8625
Gary Rutland <3715101+garyrutland@users.noreply.github.com>
2020-09-25 17:21:41 +0100

    Update plonker.rb

--- plonker.rb
+++ plonker.rb
@@ -4,7 +4,7 @@ class Plonker < Formula
   homepage 'https://github.com/applicreation/plonker'
   url 'https://github.com/applicreation/plonker/releases/download/v0.1.0/plonker-darwin-amd64-0.1.0.tar.gz'
   version '0.1.0'
-  sha256 '2c8f74b88b87cd6ea7929fc9f7bb82ba96a50b9d4d86b3248c432d31afc91789'
+  sha256 'd8a0f3292bf5ec063643bf78340b1f037f190105a750b8fadf8cd42203fda0e0'
 
   depends_on :arch => :x86_64
 
